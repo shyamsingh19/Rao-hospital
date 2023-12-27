80030f85ad258d6c305febb21a77af505492c9c5
abhishek singh <abhishekworkmail9@gmail.com>
2023-12-27 11:08:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Navbar from "./components/Navbar/navbar";
+function App() {
+  return (
+    <div className="App">
+    <h1>Abhishek</h1>
+    <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+    <div>
+        <p> navbar</p>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
