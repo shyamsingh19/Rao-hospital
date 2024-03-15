dc2e49d21eb508c66e714c80d95a6d3dee4a961c
shyam sunder singh <shyamsunders507@gmail.com>
2024-03-15 23:34:33 +0530

    home js minor update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -10,45 +10,45 @@ import Footer from "../footer/footer";
 import Header from "../header/header";
 import { Link } from "react-router-dom";
 
-
-
 const Home = () => {
   return (
     <>
-    
-  <Header />
+      <Header />
 
+      <section id="home">
+        <div className="home_parent">
+          <div className="home_wrap">
+            <div className="doctor">
+              <div className="banner_wrap">
+                <div className="divider"></div>
+                <p className="total_health">Total Health care solution</p>
+                <h1> Your most trusted health partner</h1>
+                <p className="parab">
+                  {" "}
+                  A good laugh and a long sleep are the best cures in the
+                  doctor's book.{" "}
+                </p>
+                <p className="parab">
+                  {" "}
+                  Good health is not something we can buy. However, it can be an
+                  extremely valuable savings account
+                </p>
 
-    <section id="home" >
-      <div className="home_parent">
-      <div className="home_wrap">
-
-
-        <div className="doctor">
-
-          <div className="banner_wrap">
-            <div className="divider"></div>
-            <p className="total_health">Total Health care solution</p>
-            <h1> Your most trusted health partner</h1>
-            <p className="parab"> A good laugh and a long sleep are the best cures in the doctor's book. </p>
-            <p className="parab"> Good health is not something we can buy. However, it can be an extremely valuable savings account</p>
-
-            <div className="btn-container ">
-              <Link to="/contact" className="btn">   <span>  Contact Us  </span> <MdKeyboardArrowRight className="arrowcolor" /> <span>   </span> </Link>
-              {/* <a href="/" target="_blank" className="btn" >  </a> */}
+                <div className="btn-container ">
+                  <Link to="/contact" className="btn">
+                    {" "}
+                    <span> Contact Us </span>{" "}
+                    <MdKeyboardArrowRight className="arrowcolor" />{" "}
+                    <span> </span>{" "}
+                  </Link>
+                  {/* <a href="/" target="_blank" className="btn" >  </a> */}
+                </div>
+              </div>
             </div>
-
           </div>
-
         </div>
 
-
-       
-
-      </div>
-      </div>
-
-      {/* <div className="card_section">
+        {/* <div className="card_section">
           <div className="card_item"> <FaRegClock className="cardIcon" />
             <p>Timing schedule</p>
             <h5> Working Hours</h5>
@@ -70,33 +70,24 @@ const Home = () => {
           </div>
 
         </div> */}
-    </section>
+      </section>
 
-    <section className="hesitate">
-<div className="hesitate_wrap"> 
-<p>Health Care Services </p>
-<h2> Don’t Hesitate, Contact
-  Us For Better Help & Services.</h2>
-</div>
-    </section>
+      <section className="hesitate">
+        <div className="hesitate_wrap">
+          <p>Health Care Services </p>
+          <h2> Don’t Hesitate, Contact Us For Better Help & Services.</h2>
+        </div>
+      </section>
 
-    <section className="our_testinomial">
-      <div className="testimonial_wrap">
+      <section className="our_testinomial">
+        <div className="testimonial_wrap">
+          <h3>Our Testimonials</h3>
+        </div>
+      </section>
 
-      <h3>Our Testimonials</h3>
-      </div>
-
-       
-
-    </section>
-
-<Footer />
-</>
-
+      <Footer />
+    </>
   );
-
-
-
 };
 
 export default Home;
