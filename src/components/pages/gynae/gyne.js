f265df3702b71769dbfec05f08aaca799d91894b
shyam sunder singh <shyamsunders507@gmail.com>
2024-03-16 23:35:22 +0530

    gyne.js minor update

--- src/components/pages/gynae/gyne.js
+++ src/components/pages/gynae/gyne.js
@@ -1,55 +1,70 @@
-import React from 'react'
-import Header from '../../header/header'
-import Footer from '../../footer/footer'
+import React from "react";
+import Header from "../../header/header";
+import Footer from "../../footer/footer";
 
-import "./gyne.css"
+import "./gyne.css";
 const Gyne = () => {
   return (
     <>
       <Header />
-      <section className='gyne_wrap'>
-        <p> Gynaecology deals with the health of the female reproductive system, including the vagina, uterus, ovaries, and breasts. It covers a wide range of conditions, from common issues like menstrual problems and birth control to more serious concerns like endometriosis, ovarian cancer, and uterine fibroids. </p>
+      <section className="gyne_wrap">
+        <p>
+          {" "}
+          Gynaecology deals with the health of the female reproductive system,
+          including the vagina, uterus, ovaries, and breasts. It covers a wide
+          range of conditions, from common issues like menstrual problems and
+          birth control to more serious concerns like endometriosis, ovarian
+          cancer, and uterine fibroids.{" "}
+        </p>
 
+        <p>
+          Obstetrics focuses on pregnancy, childbirth, and the postpartum
+          period. It includes prenatal care, monitoring the baby's development,
+          managing labor and delivery, and caring for the mother and baby after
+          childbirth.{" "}
+        </p>
 
-        <p>Obstetrics focuses on pregnancy, childbirth, and the postpartum period. It includes prenatal care, monitoring the baby's development, managing labor and delivery, and caring for the mother and baby after childbirth. </p>
-
-        <p>Some of the common procedures performed by gynaecologists and obstetricians include</p>
+        <p>
+          Some of the common procedures performed by gynaecologists and
+          obstetricians include
+        </p>
         <ul>
           <li>
-
-
             <p>Gynaecologists</p>
 
-            <ul>  <li>
-              Pap smears and pelvic exams
-            </li>
-              <li>IUD insertion and removal  </li>
+            <ul>
+              {" "}
+              <li>Pap smears and pelvic exams</li>
+              <li>IUD insertion and removal </li>
               <li> Colposcopy (examination of the cervix)</li>
               <li> Hysteroscopy (examination of the uterus) </li>
               <li> Laparoscopy (examination of the abdomen) </li>
-              <li> Surgery for endometriosis, ovarian cysts, and uterine fibroids
+              <li>
+                {" "}
+                Surgery for endometriosis, ovarian cysts, and uterine fibroids
               </li>
             </ul>
-
-
-
           </li>
-          <li> <p> Obstetricians </p>
+          <li>
+            {" "}
+            <p> Obstetricians </p>
             <ul>
               <li> Prenatal ultrasounds </li>
-              <li> Amniocentesis (withdrawal of amniotic fluid for testing) </li>
+              <li>
+                {" "}
+                Amniocentesis (withdrawal of amniotic fluid for testing){" "}
+              </li>
               <li> Induction of labor </li>
-              <li>  Cesarean sections</li>
+              <li> Cesarean sections</li>
               <li> Vaginal deliveries </li>
             </ul>
           </li>
         </ul>
-
       </section>
 
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Gyne
+export default Gyne;
